570e47388993b4ef368f662021533be195fe3d6b
Agung Wayudi <olipiskandar@gmail.com>
2019-08-14 16:37:34 +0700

    add all

---
+++ logika copy 2.php
@@ -0,0 +1,31 @@
+<!--
+
+cetak gol
+total cetak gol >=  10  'mendali emas' & = 20 "mendapatkan bonus 100jt"
+total cetak gol >=  7   'mendali perak'& = 9 "mendapatkan bonus 80jt"
+total cetak gol >=  4   'mendali perunggu' & = 5 "mendapatkan bonus 50jt"
+total cetak gol  <  4   'mendali emas ngambang'& "tidak mendapatkan bonus"
+-->
+
+<?php
+$gol = 110;
+
+// if ($gol >= 10) {
+//   echo "mendali emas, dengan total cetak gol : " . $gol;
+// } elseif ($gol >= 7) {
+//   echo "mendali perak, dengan total cetak gol : " . $gol;
+// } elseif ($gol >= 4) {
+//   echo "mendali perunggu, dengan total cetak gol : " . $gol;
+// } else {
+//   echo "mendali emas ngambang, dengan total cetak gol : " . $gol;
+// }
+// switch ($gol) {
+//   case 'b':
+//     echo "mendali emas, dengan total cetak gol : " . $gol;
+//     break;
+
+//   default:
+//     echo "tidak ada jawaban";
+//     break;
+// }
+?>
---
+++ x copy 2
@@ -0,0 +1,17 @@
+git config --global --unset user.name
+git config --global --unset user.email
+
+Belum Pernah Upload ke github / repo
+1. masuk ke folder project
+2. buka terminal
+3. ketik git init
+4. ketik git add .
+5. ketik git -m "pesan"
+6. ketik git remote add origin https://xxxx
+7. git push -u origin master
+
+Sudah pernah
+2. buka terminal
+4. ketik git add .
+5. ketik git -m "pesan"
+7. git push -u origin master
