94b0debd5224c3d2eb005e8735389e0107cfc88a
Agung Wahyudi <agungwahyudi@Agungs-iMac.local>
2019-08-14 13:31:04 +0700

    inisial release

---
+++ oprator.php
@@ -0,0 +1,16 @@
+<!-- operator
+aritmatika => + - * / %
+penugasan => =
+bitwise => and or xor ! left right
+perbandingan = == === != <> !== > < >= <=
+logika = && | and  !||
+string = .
+-->
+
+<?php
+// aritmatika
+$a = 1;
+$b = 10;
+$c = $a + $b;
+echo '<h5 style="color:red">' . $c . '</h5> <br> <strong>Buah</strong>';
+?>
